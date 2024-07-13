ee2297d015a4c068149e186834657ee143e3e6b0
allentran2606 <giahaot2606@gmail.com>
2024-07-13 10:31:31 +0700

    Feature: Get User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -154,4 +154,27 @@ const updateUser = async (req, res) => {
   }
 };
 
-export { signupUser, loginUser, logoutUser, followAndUnfollowUser, updateUser };
+const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username })
+      .select("-password")
+      .select("-updatedAt");
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in getUserProfile", err.message);
+  }
+};
+
+export {
+  signupUser,
+  loginUser,
+  logoutUser,
+  followAndUnfollowUser,
+  updateUser,
+  getUserProfile,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,13 @@ import {
   logoutUser,
   followAndUnfollowUser,
   updateUser,
+  getUserProfile,
 } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/profile/:username", getUserProfile);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
