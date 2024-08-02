27aba8c638b517c31406854f747c580fb82d8adf
allentran2606 <giahaot2606@gmail.com>
2024-08-02 20:33:54 +0700

    Feature: Create/Send message

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import connectDB from "./db/connectDB.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 dotenv.config();
 
@@ -30,6 +31,7 @@ app.use(cookieParser());
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
