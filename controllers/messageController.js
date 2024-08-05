d543b835d63c4e0d6b0089472391247ad9ccf1cb
allentran2606 <giahaot2606@gmail.com>
2024-08-05 09:21:23 +0700

    Feature: Add getMessage and getConversations function

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -44,4 +44,38 @@ const sendMessage = async (req, res) => {
   }
 };
 
-export { sendMessage };
+const getMessages = async (req, res) => {
+  const { otherUserId } = req.params;
+  const userId = req.user._id;
+  try {
+    const conversation = await Conversation.findOne({
+      participants: { $all: [userId, otherUserId] },
+    });
+    if (!conversation)
+      return res.status(400).json({ error: "Conversation not found" });
+
+    const messages = await Message.find({
+      conversationId: conversation._id,
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getConversations = async (req, res) => {
+  const userId = req.user._id;
+
+  try {
+    const conversations = await Conversation.find({
+      participants: userId,
+    }).populate({ path: "participants", select: "username profilePicture" });
+
+    res.status(200).json(conversations);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { sendMessage, getMessages, getConversations };
