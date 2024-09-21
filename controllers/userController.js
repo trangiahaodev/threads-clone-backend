26c661eee3246022891629d0341015572251fcf8
allentran2606 <giahaot2606@gmail.com>
2024-09-21 22:55:23 +0700

    Feature: Add suggested users section

--- controllers/userController.js
+++ controllers/userController.js
@@ -219,6 +219,36 @@ const getUserProfile = async (req, res) => {
   }
 };
 
+const getSuggestedUsers = async (req, res) => {
+  try {
+    // Remove current user and users being followed from suggestedUsers array
+    const userId = req.user._id;
+    const usersFollowedByCurrentUser = await User.findById(userId).select(
+      "following"
+    );
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      {
+        $sample: { size: 10 },
+      },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByCurrentUser.following.includes(user._id)
+    );
+    const suggestedUsers = filteredUsers.slice(0, 4);
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 export {
   signupUser,
   loginUser,
@@ -226,4 +256,5 @@ export {
   followAndUnfollowUser,
   updateUser,
   getUserProfile,
+  getSuggestedUsers,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,12 +7,14 @@ import {
   followAndUnfollowUser,
   updateUser,
   getUserProfile,
+  getSuggestedUsers,
 } from "../controllers/userController.js";
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/profile/:query", getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
