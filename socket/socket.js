544be286ab038a620cad2c2616082a446a0e119b
allentran2606 <giahaot2606@gmail.com>
2024-11-10 10:46:29 +0700

    checkpoint before deploy monerepo

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://threads-clone-db2i.onrender.com",
+    origin: "localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   },
